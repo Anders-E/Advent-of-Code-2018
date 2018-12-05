a7998d9ed4bb606c0fdcfbb85bbc83e021179640
Anders Eriksson <aeri3@kth.se>
2018-12-05 01:54:19 +0100

    solve day 3 pt 1

--- day_3/day_3_1.py
+++ day_3/day_3_1.py
@@ -6,20 +6,20 @@ def mark_fabric(x_margin, y_margin, w, h):
         for y in range(y_margin, y_margin + h):
             fabric[x][y] += 1
 
+
 def overlaps():
     count = 0
-    for x in fabric:
-        for y in x:
-            if y > 1:
+    for x in range(1000):
+        for y in range(1000):
+            if fabric[x][y] > 1:
                 count += 1
     return count
 
 regex = re.compile(r"#(\d*) @ (\d*),(\d*): (\d*)x(\d*)")
-fabric = [[0] * 1000] * 1000
+fabric = [[0] * 1000 for x in range(1000)]
 
 for line in stdin.readlines():
     id, x_margin, y_margin, w, h = map(int, re.match(regex, line).groups())
     mark_fabric(x_margin, y_margin, w, h)
 
 print(overlaps())
-
