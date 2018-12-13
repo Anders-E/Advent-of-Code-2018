1038da40234f4d35e3dbf8f38ee480af7699d38d
Anders Eriksson <aeri3@kth.se>
2018-12-13 16:30:58 +0100

    start work on day 13 pt 1

---
+++ day_13/day_13_1.py
@@ -0,0 +1,107 @@
+from sys import stdin
+from copy import deepcopy
+
+class Track:
+    def __init__(self, track_map):
+        self.map = []
+        for line in track_map:
+            self.map.append(list(line.rstrip()))
+        
+        self.carts = []
+        for y, line in enumerate(self.map):
+            for x, tile in enumerate(line):
+                if tile == '<' or tile == '>':
+                    self.set_tile(x, y, '-')
+                    self.carts.append(self.Cart(x, y, tile))
+                if tile == '^' or tile == 'v':
+                    self.set_tile(x, y, '|')
+                    self.carts.append(self.Cart(x, y, tile))
+
+    def move_carts(self):
+        for cart in self.carts:
+            cart.move(track)
+    
+    def set_tile(self, x, y, tile):
+        self.map[y][x] = tile
+
+    def get_tile(self, x, y):
+        return self.map[y][x]
+
+    def print_map(self):
+        tmp_map = deepcopy(self.map)
+        for cart in self.carts:
+            tmp_map[cart.y][cart.x] = cart.direction
+        for line in tmp_map:
+            print(''.join(line))
+
+    class Cart:
+        turn_right = {
+            '^' : '>',
+            '>' : 'v',
+            'v' : '<',
+            '<' : '^'
+        }
+        turn_left = {
+            '^' : '<',
+            '<' : 'v',
+            'v' : '>',
+            '>' : '^'
+        }
+
+        def __init__(self, x, y, direction):
+            self.x = x
+            self.y = y
+            self.direction = direction
+            self.intersection = 0
+        
+        def move(self, track):
+            tile = track.get_tile(self.x, self.y)
+            if tile == '+' or tile == '/' or tile == '\\':
+                self.turn(tile)
+            
+            if self.direction == '>':
+                self.x += 1
+            if self.direction == '<':
+                self.x -= 1
+            if self.direction == '^':
+                self.y -= 1
+            if self.direction == 'v':
+                self.y += 1
+
+        def turn(self, tile):
+            if tile == '/':
+                if self.direction == '>':
+                    self.direction = '^'
+                elif self.direction == '<':
+                    self.direction = 'v'
+                elif self.direction == '^':
+                    self.direction = '>'
+                elif self.direction == 'v':
+                    self.direction = '<'
+            if tile == '\\':
+                if self.direction == '>':
+                    self.direction = 'v'
+                elif self.direction == '<':
+                    self.direction = '^'
+                elif self.direction == '^':
+                    self.direction = '<'
+                elif self.direction == 'v':
+                    self.direction = '>'
+            if tile == '+':
+                if self.intersection == 0:
+                    self.direction = self.turn_left[self.direction]
+                if self.intersection == 2:
+                    self.direction = self.turn_right[self.direction]
+                self.intersection = (self.intersection + 1) % 3
+
+track = Track(stdin.readlines())
+while True:
+    #track.print_map()
+    track.move_carts()
+    cart_positions = set()
+    for cart in track.carts:
+        cart_position = (cart.x, cart.y)
+        if cart_position in cart_positions:
+            print(cart_position)
+            exit()
+        cart_positions.add(cart_position)
